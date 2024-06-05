0e5a638f60c20affc0a7518d9bf81927856ab0c9
Gabriella Koos <koos.gabriella92@gmail.com>
2024-06-05 10:20:21 +0200

    add home page layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,27 @@
+import logo from "./assets/no-projects.png";
+import ProjectsSidebar from "./components/ProjectsSidebar";
+
 function App() {
   return (
-    <>
-      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
-    </>
+    <main className="h-screen my-8 flex gap-8">
+      <ProjectsSidebar />
+      <div className="mt-24 text-center w-2/3">
+        <img
+          src={logo}
+          alt="Notepad with pen"
+          className="w-16 h-16 object-contain mx-auto"
+        />
+        <h2 className="text-xl font-bold text-stone-500 my-4">
+          No Project Selected
+        </h2>
+        <p className="mb-4 text-stone-400">
+          Select a project or get started with a new one
+        </p>
+        <button className="px-4 py-2 text-xs md:text-base rounded-md bg-stone-700 text-stone-400 hover:bg-stone-600 hover:text-stone-100">
+          Create new project
+        </button>
+      </div>
+    </main>
   );
 }
 
